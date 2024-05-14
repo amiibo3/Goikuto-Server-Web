e1f63ef375336047334698f444e85b5aba32d6a6
amiibo3 <134783611+amiibo3@users.noreply.github.com>
2024-05-14 16:32:40 +0900

    update

--- slider.js
+++ slider.js
@@ -1,4 +1,8 @@
 $('.slider').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
+  arrows: true,
+  appendArrows: $('.arrow_box'),
+  prevArrow: '<div class="slide-arrow prev-arrow"></div>',
+  nextArrow: '<div class="slide-arrow next-arrow"></div>'
 });
