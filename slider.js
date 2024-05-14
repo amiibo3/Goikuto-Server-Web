04b8518a770c6285029533b6d0243a27fa638992
amiibo3 <134783611+amiibo3@users.noreply.github.com>
2024-05-14 15:27:03 +0900

    update

--- slider.js
+++ slider.js
@@ -1,9 +1,9 @@
 $(function() {
   $('.slider').slick({
-    autoplay: true, // 自動再生ON
-    dots: true, // ドットインジケーターON
-    centerMode: true, // 両サイドに前後のスライド表示
-    centerPadding: '0px', // 左右のスライドのpadding
-    slidesToShow: 3, // 一度に表示するスライド数
+    autoplay: true,
+    dots: true,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
   });
 });
