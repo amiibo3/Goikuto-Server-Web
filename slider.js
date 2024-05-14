2be8dbe54da02f11ba18daaae3be89e0c03fe2ea
amiibo3 <134783611+amiibo3@users.noreply.github.com>
2024-05-14 15:16:54 +0900

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementByClassName("slider").slick({
-    autoplay: true, 
-    dots: true, 
-    centerMode: true, 
-    centerPadding: '0px', 
-    slidesToShow: 3, 
+$(function() {
+  $('.slider').slick({
+    autoplay: true, // 自動再生ON
+    dots: true, // ドットインジケーターON
+    centerMode: true, // 両サイドに前後のスライド表示
+    centerPadding: '0px', // 左右のスライドのpadding
+    slidesToShow: 3, // 一度に表示するスライド数
   });
 });
